4b91dfb58c7da051117bf1bf642e377bd5e0b663
JamesAnunda <cjanunda@gmail.com>
2020-01-25 04:01:21 -0500

    Setup Contact page

--- pages/contact.js
+++ pages/contact.js
@@ -3,10 +3,31 @@ import Layout from '../components/layout';
 
 export default function Index() {
     return (
-      <div>
-        <Layout>
-            <p>Contact us here @</p>
-        </Layout>
-      </div>
+      <div> {/* Main Div */}
+        <Layout />
+        <h1>Contact us here </h1>
+        <div>
+        <form method="post" action="#" className-="contactForum">
+            <label>
+                Name
+                <input type="text" name="name" id="name" />
+            </label>
+            <label>
+                Email
+                <input type="email" name="email" id="email" />
+            </label>
+            <label>
+                Subject
+                <input type="text" name="subject" id="subject" />
+            </label>
+            <label>
+                Message
+                <textarea name="message" id="message" rows="5" />
+            </label>
+            <button type="submit">Send</button>
+            <input type="reset" value="Clear" />
+            </form>
+        </div>
+      </div>/* end of main div */
     );
   }
