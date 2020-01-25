da007552528868ec6257ea5a2c049afcfbf8d712
JamesAnunda <cjanunda@gmail.com>
2020-01-24 23:30:53 -0500

    Remove about folder
    remove about folder and left it in pages

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Index = () => (
     <div>
-      <Link href="../index">
+      <Link href="./index">
         <a title="Home">Home page</a>
       </Link>
       <p>This is the about page.</p>
